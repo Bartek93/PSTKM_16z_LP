cb3a35fad88d71a97cb7db7f32beeeb566a6948b
Tomasz Osinski <osinstom@gmail.com>
2016-12-12 13:07:09 -0800

    Added: parse() method in Parser + resources folder with trivial scenario

--- src/pl/edu/LinkPathMain.java
+++ src/pl/edu/LinkPathMain.java
@@ -4,6 +4,7 @@ public class LinkPathMain {
 
 	public static void main(String[] args) {
 		Model model = new Model();
+		Parser.parse("resources/trivial.txt", 0, 2, 2);
 	}
 	
 }
--- src/pl/edu/Parser.java
+++ src/pl/edu/Parser.java
@@ -1,11 +1,53 @@
 package pl.edu;
 
+import java.util.List;
+
+import edu.asu.emit.algorithm.graph.Path;
+import edu.asu.emit.algorithm.graph.Graph;
+import edu.asu.emit.algorithm.graph.shortestpaths.YenTopKShortestPathsAlg;
+
 public class Parser {
-	
-	public Parser() {
-		
+
+	public static Integer[] parse(String dataFileName, int srcNode,
+			int dstNode, int numberOfPaths) {
+
+		Graph graph = new Graph(dataFileName);
+		System.out.println(graph.getVertexList());
+		YenTopKShortestPathsAlg yenAlg = new YenTopKShortestPathsAlg(graph);
+		List<Path> shortest_paths_list = yenAlg.getShortestPaths(
+				graph.getVertex(srcNode), graph.getVertex(dstNode),
+				numberOfPaths);
+		System.out.println(":" + shortest_paths_list);
+
+		// Object x = shortest_paths_list.toArray();
+		// System.out.println(yenAlg.getResultList().size());
+
+//		if (shortest_paths_list.size() < numberOfPaths) {
+//			throw new ExceptionInInitializerError();
+//		}
+
+		String path = shortest_paths_list.get(numberOfPaths - 1)
+				.getVertexList().toString();
+
+		System.out.println("From: " + srcNode + " To: " + dstNode + "  - "
+				+ path);
+
+		String[] tokens = path.split(":");
+		String[] items = tokens[0].replaceAll("\\[", "").replaceAll("\\]", "")
+				.replaceAll(" ", "").split(",");
+
+		Integer[] results = new Integer[items.length];
+
+		for (int i = 0; i < items.length; i++) {
+			try {
+				results[i] = Integer.parseInt(items[i]);
+				// System.out.print(results[i].toString());
+				// System.out.println();
+			} catch (NumberFormatException nfe) {
+				System.err.println(nfe.toString());
+			}
+		}
+		return results;
 	}
-	
-	
-	
+
 }
