67e2fa5e54c78220b6eeec4ef12f10e4b6d922a9
Tomasz Osinski <osinstom@gmail.com>
2016-12-17 09:09:04 -0800

    edge list parser

--- src/pl/edu/Parser.java
+++ src/pl/edu/Parser.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Map;
 
 import pl.edu.pojo.Demand;
+import pl.edu.pojo.Edge;
 import edu.asu.emit.algorithm.graph.Path;
 import edu.asu.emit.algorithm.graph.Graph;
 import edu.asu.emit.algorithm.graph.abstraction.BaseVertex;
@@ -43,16 +44,21 @@ public class Parser {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+		// load graph
 		Graph graph = new Graph(graphFileName);
 		System.out.println("\nVertices: " + graph.getVertexList());
-		System.out.println("\nInitial load: ");
-		Map<Pair<Integer, Integer>, Integer> edges = graph.getEdges();
-		for (Pair<Integer, Integer> pair : edges.keySet()) {
-			System.out.println("Edge " + pair.first() + " - " + pair.second()
-					+ ": " + edges.get(pair));
+		
+		// get intial loads and fill list of edges
+		Map<Pair<Integer, Integer>, Integer> edgesMap = graph.getEdges();
+		List<Edge> edges = new ArrayList<Edge>();
+		int count = 1;
+		for (Pair<Integer, Integer> pair : edgesMap.keySet()) {
+			Edge edge = new Edge(count, pair.first(), pair.second(), edgesMap.get(pair));
+			edges.add(edge);
+			count++;
 		}
 
+		// get list of paths for every demand
 		for (Demand d : demands) {
 			YenTopKShortestPathsAlg yenAlg = new YenTopKShortestPathsAlg(graph);
 			List<Path> shortest_paths_list = yenAlg.getShortestPaths(
@@ -63,37 +69,7 @@ public class Parser {
 			System.out.println("Shortest paths:" + shortest_paths_list);
 			map.put(d, shortest_paths_list);
 		}
-
-		// Object x = shortest_paths_list.toArray();
-		// System.out.println(yenAlg.getResultList().size());
-
-		// if (shortest_paths_list.size() < numberOfPaths) {
-		// throw new ExceptionInInitializerError();
-		// }
-
-		// String optimalVertexList = shortest_paths_list.get(numberOfPaths - 1)
-		// .getVertexList().toString();
-		//
-		// System.out.println("Optimal path from: " + srcNode + " to: " +
-		// dstNode
-		// + "  - " + optimalVertexList);
-
-		// String[] tokens = optimalVertexList.split(":");
-		// String[] optimalVertexTab = tokens[0].replaceAll("\\[", "")
-		// .replaceAll("\\]", "").replaceAll(" ", "").split(",");
-		//
-		// Integer[] results = new Integer[optimalVertexTab.length];
-		//
-		// for (int i = 0; i < optimalVertexTab.length; i++) {
-		// try {
-		// results[i] = Integer.parseInt(optimalVertexTab[i]);
-		// System.out.print(results[i].toString());
-		// System.out.println();
-		// } catch (NumberFormatException nfe) {
-		// System.err.println(nfe.toString());
-		// }
-		// }
-
+		
 		return map;
 	}
 
