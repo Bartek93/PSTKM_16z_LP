77760aefa364f54ee405f92ef18366ec1a1399f5
Paweł <pczar93@gmail.com>
2016-12-18 03:56:05 +0100

    console menu added, we can add also scanner for modularity and num of paths

--- src/pl/edu/LinkPathMain.java
+++ src/pl/edu/LinkPathMain.java
@@ -2,6 +2,7 @@ package pl.edu;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 import edu.asu.emit.algorithm.graph.Path;
 import pl.edu.pojo.CplexInput;
@@ -16,11 +17,26 @@ public class LinkPathMain {
 		int numberOfPaths = 5;
 		
 		System.out.println("---------- 1st example ----------");
-//		CplexInput input  = Parser.parse("resources/trivial.txt", "resources/demands_trivial.txt", numberOfPaths);
-		
-		CplexInput input  = Parser.parse("resources/non-trivial_1.txt", "resources/demands_nontrivial1.txt", numberOfPaths);
-		
-		
+                
+                CplexInput input = null;
+                String scenario = printMenu();
+                String dumpFileName = "";
+                if (scenario.equals("1"))
+                {
+                    input = Parser.parse("resources/trivial.txt", "resources/demands_trivial.txt", numberOfPaths);
+                    dumpFileName = "trivial_dump.txt";
+                }
+		else if (scenario.equals("2"))
+                {
+                    input  = Parser.parse("resources/non-trivial_1.txt", "resources/demands_nontrivial1.txt", numberOfPaths);
+                    dumpFileName = "nontrivial1_dump.txt";
+                }
+		else
+                {
+                    input  = Parser.parse("resources/non-trivial_2.txt", "resources/demands_nontrivial2.txt", numberOfPaths); 
+                    dumpFileName = "nontrivial2_dump.txt";
+                }
+                    
 		System.out.print("--- Edges ---");
                 for (Edge e : input.getEdges())
                     System.out.print(e);
@@ -29,27 +45,27 @@ public class LinkPathMain {
 		System.out.print(input.getDemandPathsMap());
 		
 		
-//		// 1st
-//		Parser.dumpToFile(input.getDemandPathsMap(), "trivial_dump.txt");
-		// 2nd 
-		Parser.dumpToFile(input.getDemandPathsMap(), "nontrivial1_dump.txt");
+		Parser.dumpToFile(input.getDemandPathsMap(), dumpFileName);
 		
 		model.createModel(input, numberOfPaths);
 		
-		
-//		System.out.println("---------- 2nd example ----------");
-//		Parser.parse("resources/non-trivial_1.txt", 1, 3, 4);
-//		Parser.parse("resources/non-trivial_1.txt", 1, 4, 4);
-//
-//		System.out.println("---------- 3rd example ----------");
-//		Parser.parse("resources/non-trivial_2.txt", 6, 0, 4);
-//		Parser.parse("resources/non-trivial_2.txt", 10, 4, 9); // it doesn't get
-//																// the shortest,
-//																// it gets last
-//																// in list
-//		Parser.parse("resources/non-trivial_2.txt", 2, 9, 4);
-		
-		
-		
 	}
+        public static String printMenu()
+        {
+            String choice = "a";
+            do
+            {
+                System.out.println("*****************************************************************************");
+                System.out.println("Wybierz scenariusz ktory chcesz uruchomić poprzez podanie wybranej liczby:");
+                System.out.println("1. Scenariusz trywialny");
+                System.out.println("2. Scenariusz nietrywialny 1");
+                System.out.println("3. Scenariusz nietrywialny 2");
+                System.out.println("Your choice: ");
+                Scanner scan = new Scanner(System.in);
+                choice = scan.nextLine();
+            }
+            while(!choice.equals("1") && !choice.equals("2") && !choice.equals("3"));
+
+            return choice;
+        }
 }
